4388598691133d3e4728af7331611ac93d8c6c7d
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2015-03-21 20:41:13 +0530

    Changes on Landing page with ng repeat

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -5,5 +5,36 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+		$scope.alerts = [{
+			iconCls: 'glyphicon-user',
+			btnStyle: 'btn-success',
+			val: 'XXXXX',
+			note: 'Some Note Here'
+		},{
+			iconCls: 'glyphicon-th',
+			btnStyle: 'btn-danger',
+			val: 'XXXXX',
+			note: 'Some Note Here'
+		},{
+			iconCls: 'glyphicon-thumbs-up',
+			btnStyle: 'btn-danger',
+			val: 'XXXXX',
+			note: 'Some Note Here'
+		},{
+			iconCls: 'glyphicon-envelope',
+			btnStyle: 'btn-warning',
+			val: 'XXXXX',
+			note: 'Some Note Here'
+		},{
+			iconCls: 'glyphicon-retweet',
+			btnStyle: 'btn-success',
+			val: 'XXXXX',
+			note: 'Some Note Here'
+		},{
+			iconCls: 'glyphicon-globe',
+			btnStyle: 'btn-primary',
+			val: 'XXXXX',
+			note: 'Some Note Here'
+		}, ];
 	}
 ]);
