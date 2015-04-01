ec7664fb55192adc68e0f3a0b887b6aa058464c3
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2015-04-01 13:49:34 +0530

    fontawesome added

--- config/env/all.js
+++ config/env/all.js
@@ -16,6 +16,7 @@ module.exports = {
 				'public/lib/bootstrap/dist/css/bootstrap.css',
 //				'public/lib/bootswatch-dist/css/bootstrap.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'public/lib/fontawesome/css/font-awesome.css'
 			],
 			js: [
 				'public/lib/angular/angular.js',
--- config/env/production.js
+++ config/env/production.js
@@ -8,6 +8,7 @@ module.exports = {
 //			css: [
 //				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 //				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+//				'public/lib/fontawesome/css/font-awesome.min.css'
 //			],
 //			js: [
 //				'public/lib/angular/angular.min.js',
