6681d7f697c3330a37d9f8b1b0feec411d9a4ed8
Suresh Sivanantham <suresh.sivanantham@gmail.com>
2015-04-28 17:22:31 +0530

    one more

--- public/modules/incexps/services/app.incexp.chart.client.service.js
+++ public/modules/incexps/services/app.incexp.chart.client.service.js
@@ -1,15 +1,40 @@
 'use strict';
 
-angular.module('incexps').service('ChartService', [ '$http', '$q',
-	function($http, $q) {
+angular.module('incexps').service('ChartService', [ '$http', '$q', '$stateParams',
+	function($http, $q, $stateParams) {
 		var chartService = {};
 		chartService.transformToHeatMapData = function(incexps){
+			
 			var data = {};
+			
+		    var start = null;
+		    var end = null;
+		    if($stateParams.month && $stateParams.year){
+		        start = new Date($stateParams.year, $stateParams.month-1);
+		        end = new Date($stateParams.year, $stateParams.month);
+		    }
+	    	var currentDate = new Date(start);//pull and store start date here
+	    	var currentWeek = 1;
+		    while(true){
+		    	if(currentDate >= start && currentDate < end){
+		    		var dataArr = [];
+		    		dataArr.push(currentDate.getDay());
+		    		dataArr.push(currentWeek);
+		    		
+		    		if(currentDate.getDay() === 6){
+		    			currentWeek++;	//reached day Saturday - so moving pointer to next week		
+		    		}
+		    	} else {
+		    		break;
+		    	}
+		    	currentDate.setDate(currentDate.getDate() + 1);	//	moving the pointer to next date
+		    }
+		
 			data.xAxis = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 			data.yAxis = ['Week-1', 'Week-2', 'Week-3', 'Week-4'];
-			data.seriesData = [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], 
+			data.seriesData = [[0, 1, 19], [0, 2, 8], [0, 3, 24], 
 			                   [1, 0, 'NA'], [1, 1, 58], [1, 2, 78], [1, 3, 117], 
-			                   [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64],
+			                   [2, 0, 35], [2, 2, 123], [2, 3, 64],
 			                   [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19],
 			                   [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117],
 			                   [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6],
